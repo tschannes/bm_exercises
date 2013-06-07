9aa5880cdc23f380947a59f282044065857f5e3f
Jin <hojin.yoo@outlook.com>
2013-06-07 12:46:21 -0400

    first commit

---
+++ lunchbot_run.rb
@@ -0,0 +1,15 @@
+require "socket"
+require_relative "lunchbot"
+
+server = "chat.freenode.net"
+port = "6667"
+nick = "LunchBot"
+channel = "#bitmakerlabs"
+greeting_prefix = "PRIVMSG #bitmakerlabs"
+
+s = TCPSocket.open(server, port)
+
+s.puts "USER LunchBot 0 * LunchBot"
+s.puts "NICK #{nick}"
+s.puts "JOIN #{channel}"
+s.puts "PRIVMSG #{channel} : Hello from Lunch Bot"
