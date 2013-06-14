666eb76dd531295ad14ed26536fd05c5ee99b3ae
tschannes <mail@tscherrig.net>
2013-06-14 00:23:30 -0400

    scraping piratbay to find biggest movie ever

---
+++ pirate/html_maker.rb
@@ -0,0 +1,4 @@
+html = File.new("index.html", "w+")
+fileHtml.puts "<HTML><BODY BGCOLOR='green'>"
+
+html.close()
---
+++ pirate/pirate.rb
@@ -0,0 +1,74 @@
+require 'Nokogiri'
+require 'Open-Uri'
+require 'pry'
+require 'CSV'
+
+class NoAccessError < Exception
+end
+ 
+
+
+links = []
+sizes = []
+names = []
+url_base = "http://thepiratebay.sx"
+
+#getting the 100 most downloaded movies from piratebay
+doc = Nokogiri::HTML(open('http://thepiratebay.sx/top/201'))
+
+url = doc.css('a.detLink').map { |link| link['href'] }
+
+links = url.map do |url|
+	url = url_base + url
+end
+
+# puts links.length
+# puts links.inspect
+# puts links[0]
+puts "starting to crawl:"
+
+
+#getting movie sizes from subsites
+i = 1
+links.each do |link|
+	puts "scraping page Nr #{i}"
+	begin
+		doc = Nokogiri::HTML(open("#{link}"))
+	rescue Exception=>e
+		puts "Error: #{e}"
+		sleep 2
+	else
+		puts "reading #{link}"
+		size = doc.css('div#detailsframe div#details dl.col1 dd')[2].text
+		size = size.scan(/(\d+\WBytes)/).flatten
+		sizes << size
+
+		name = doc.css('div#title').text.strip
+		names << name
+		i += 1
+	ensure
+		sleep 1 + rand
+	end
+end
+
+puts "end of crawl"
+puts ""
+puts "Sizes: #{sizes.inspect}"
+puts "Names: #{names.inspect}"
+data = names.zip(sizes)
+puts data.inspect
+
+#sorting the data
+sorted_data = data.sort_by{|x,y|y}
+
+
+#piping data to file
+CSV.open('data.csv', 'wb') do |row|
+  sorted_data.each do |d|
+    row << d
+  end
+end
+
+#regex for bytes-extraction
+# string.scan(/(\d+\WBytes)/)
+# => [["1231734066 Bytes"], ["1238182247 Bytes"]]
