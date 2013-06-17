0ede926a1668099f782855e9c5af19478e21a5a0
tschannes <mail@tscherrig.net>
2013-06-17 14:33:47 -0400

    guess.js updated

--- js/guess.js
+++ js/guess.js
@@ -2,16 +2,20 @@ var target = ["t", "a", "r", "g", "e", "t"];
 var board = [];
 
 function guessLetter(letter) {
+	letter = letter.toLowerCase();
 	for (i = 0; i < target.length; i++) {
 		if (letter == target[i]) {
 			board[i] = target[i];
-			console.log("you've got one letter down.");
+			if(target.length===board.length){
+				console.log("You have won!!!");
+			}else{
+				console.log("you've got one letter down. keep going");
+			}
 		} else if(letter == "_"){
 			board[i] = "_"
 			console.log("you have missed. Try once more.");
 		}
 	}
-	console.log("This is being run.");
-	console.log("You've guessed: " + board.join(""));
+	console.log(board);
 }
 guessLetter("R");
